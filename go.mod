c9ea6744368dcf57a7ba34e2fad71f5ca35e3102
Emma <emma.a.kellam@gmail.com>
2024-09-24 15:33:41 -0400

    Created my blank main.go file to start writing my program logic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module habithatcher-app
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+- I will uncomment this import statement once my code has been written
+- the linter keeps screaming at me because nothing has been used yet lol
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+*/
+
+/* TODO MAIN PROGRAM */
